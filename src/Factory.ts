93e8b59d2d0aa21ad7533d0787e3e295ec115127
bbachain <bbachain123@gmail.com>
2023-04-11 15:54:20 +0300

    chore: added tiny-secp256k1 and bip32 to derive account

--- src/Factory.ts
+++ src/Factory.ts
@@ -1,7 +1,7 @@
 import * as bip39 from "bip39";
-// import BIP32Factory, { BIP32Interface } from 'bip32';
-// import * as ecc from 'tiny-secp256k1';
-// const bip32 = BIP32Factory(ecc);
+import BIP32Factory, { BIP32Interface } from 'bip32';
+import * as ecc from 'tiny-secp256k1';
+const bip32 = BIP32Factory(ecc);
 
 export const createAccount = async (id: string, name: string, strength = 128) => {
   const mnemonic = bip39.generateMnemonic(strength);
@@ -17,7 +17,6 @@ export const createAccount = async (id: string, name: string, strength = 128) =>
 
 export const derivationAccount = (seed: string, path: string) => {
   const buffer = Buffer.from(seed, 'hex');
-  // const root: BIP32Interface = bip32.fromSeed(buffer);
-  // return root.derivePath(path);
-  return buffer.toString('hex');
+  const root: BIP32Interface = bip32.fromSeed(buffer);
+  return root.derivePath(path);
 };
